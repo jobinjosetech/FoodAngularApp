2c0725f0ab74140e8c193f52fb0fa7742f6bcb5b
jobin <jobinjose.in@gmail.com>
2022-11-25 08:14:30 +0530

    burger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { BurgerComponent } from './burger/burger.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    BurgerComponent,
   ],
   imports: [
     BrowserModule,
